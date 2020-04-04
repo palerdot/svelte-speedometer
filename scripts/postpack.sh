69937194299c3f45c5cabd386031684bb6341c20
Arun <palerdot@gmail.com>
2020-04-04 22:17:58 +0530

    fix symlink in npm postpack scripts

--- scripts/postpack.sh
+++ scripts/postpack.sh
@@ -4,5 +4,5 @@
 echo "=> removing freshly copied CORE files and replacing SYMLINKS"
 rm -rf src/core
 echo "=> and restoring SYMLINKS"
-ln -s lib/d3-speedometer/src/core src/
+ln -s ../lib/d3-speedometer/src/core src/
 echo "=> Restored symlinked CORE"
