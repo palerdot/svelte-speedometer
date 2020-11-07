b8484c93ce52f2dfb42bfbca4c646158e86ce2e8
palerdot <palerdot@gmail.com>
2020-11-07 21:09:24 +0530

    feat(types): Transition type for typescript

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,36 @@
 declare module "svelte-speedometer" {
+  enum Transition {
+    easeLinear = "easeLinear",
+    easeQuadIn = "easeQuadIn",
+    easeQuadOut = "easeQuadOut",
+    easeQuadInOut = "easeQuadInOut",
+    easeCubicIn = "easeCubicIn",
+    easeCubicOut = "easeCubicOut",
+    easeCubicInOut = "easeCubicInOut",
+    easePolyIn = "easePolyIn",
+    easePolyOut = "easePolyOut",
+    easePolyInOut = "easePolyInOut",
+    easeSinIn = "easeSinIn",
+    easeSinOut = "easeSinOut",
+    easeSinInOut = "easeSinInOut",
+    easeExpIn = "easeExpIn",
+    easeExpOut = "easeExpOut",
+    easeExpInOut = "easeExpInOut",
+    easeCircleIn = "easeCircleIn",
+    easeCircleOut = "easeCircleOut",
+    easeCircleInOut = "easeCircleInOut",
+    easeBounceIn = "easeBounceIn",
+    easeBounceOut = "easeBounceOut",
+    easeBounceInOut = "easeBounceInOut",
+    easeBackIn = "easeBackIn",
+    easeBackOut = "easeBackOut",
+    easeBackInOut = "easeBackInOut",
+    easeElasticIn = "easeElasticIn",
+    easeElasticOut = "easeElasticOut",
+    easeElasticInOut = "easeElasticInOut",
+    easeElastic = "easeElastic",
+  }
+
   enum CustomSegmentLabelPosition {
     Outside = "OUTSIDE",
     Inside = "INSIDE",
@@ -56,7 +88,7 @@ declare module "svelte-speedometer" {
   }
 
   // named exports of all the types
-  export { Props, CustomSegmentLabel, CustomSegmentLabelPosition }
+  export { Props, CustomSegmentLabel, CustomSegmentLabelPosition, Transition }
 
   interface ComponentOptions {
     target: HTMLElement
