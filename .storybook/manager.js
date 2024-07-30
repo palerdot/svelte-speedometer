433542ea21f2384d990aea3657f646977ae2a97d
palerdot <palerdot@gmail.com>
2024-07-30 20:33:05 +0530

    feat: storybook v8

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,16 @@
-import { addons } from "@storybook/addons"
-import { themes } from "@storybook/theming"
+import { addons } from "@storybook/manager-api"
+import { themes, create } from "@storybook/theming/create"
+import theme from "../src/core/theme"
+
+const speedoTheme = create({
+  ...themes.dark,
+  ...theme,
+
+  brandTitle: "svelte-speedometer",
+  brandUrl: "https://github.com/palerdot/svelte-speedometer",
+})
 
 addons.setConfig({
   showPanel: true,
-  theme: themes.dark,
+  theme: speedoTheme,
 })
